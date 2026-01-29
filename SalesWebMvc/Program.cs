fa458a82afc7365cca926047d1b9c9335b6a6533
Jonatas Da Silva Prado <jonatasprado445@gmail.com>
2026-01-29 15:45:33 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
