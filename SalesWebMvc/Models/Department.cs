e94ef9c2c743a3cc1877d688f0d39f127ed25e74
Jonatas Da Silva Prado <jonatasprado445@gmail.com>
2026-01-28 21:44:57 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,32 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using NuGet.Protocol.Plugins;
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller>  Sellers{ get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        //nao se inclui atributos que são coleções 
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void addSaller(Seller seller)
+        {
+             Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial,DateTime finail)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial,finail));
+        }
     }
 }
